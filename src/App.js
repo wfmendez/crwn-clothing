a789b6d458c41e943d5eef2312caa96b9da69c5b
wfmendez <wmendezgarcia@byupathway.edu>
2025-08-22 21:33:51 -0400

    feat: Add Nav bar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>A</h1>
+      </div>
+      <Outlet />
+    </div>
+  )
+}
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 };
