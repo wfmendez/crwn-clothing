1806e93cbd5ebc980b4bd6202d0dca3b858b5eab
wfmendez <wmendezgarcia@byupathway.edu>
2025-08-26 21:39:41 -0400

    feat: Add Navigation Route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
+import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
 
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>A</h1>
-      </div>
-      <Outlet />
-    </div>
-  )
-}
 
 const App = () => {
   return (
