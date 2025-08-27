a23cbdea745068b9993b04e285fa737d9fd17e7b
wfmendez <wmendezgarcia@byupathway.edu>
2025-08-27 14:38:35 -0400

    style: Add style for navigation and logo

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,16 @@
 import { Fragment } from "react/jsx-runtime";
 import { Outlet, Link } from "react-router-dom"
 
+import './navigation.styles.scss'
+
+import { ReactComponent as CrwnLogo} from '../../assets/crown.svg'
+
 const Navigation = () => {
   return (
     <Fragment>
       <div className="navigation">
               <Link className="logo-container" to='/'>
-                  <div>Logo</div>
+                  <CrwnLogo className="logo" />
               </Link>
               <div className="nav-links-container">
                   <Link className="nav-link" to='/shop'>
